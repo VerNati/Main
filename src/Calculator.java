4610bb606d3cb46377711a655e1a07ece64b0c48
VerNati <145490234+VerNati@users.noreply.github.com>
2023-09-19 22:12:06 +0300

    release 2.0

--- src/Calculator.java
+++ src/Calculator.java
@@ -7,42 +7,7 @@ public class Calculator {
 
         try {
             String input = scanner.nextLine();
-            String[] parts = input.split(" ");
-
-            if (parts.length != 3) {
-                throw new IllegalArgumentException("Неверный формат ввода. Используйте пример: 2 + 3");
-            }
-
-            int a = Integer.parseInt(parts[0]);
-            char operator = parts[1].charAt(0);
-            int b = Integer.parseInt(parts[2]);
-
-            if (a < 1 || a > 10 || b < 1 || b > 10) {
-                throw new IllegalArgumentException("Числа должны быть от 1 до 10.");
-            }
-
-            int result = 0;
-
-            switch (operator) {
-                case '+':
-                    result = a + b;
-                    break;
-                case '-':
-                    result = a - b;
-                    break;
-                case '*':
-                    result = a * b;
-                    break;
-                case '/':
-                    if (b == 0) {
-                        throw new ArithmeticException("Деление на ноль невозможно.");
-                    }
-                    result = a / b;
-                    break;
-                default:
-                    throw new IllegalArgumentException("Неверная операция. Используйте +, -, *, /.");
-            }
-
+            int result = calc(input);
             System.out.println("Результат: " + result);
 
         } catch (NumberFormatException e) {
@@ -53,5 +18,44 @@ public class Calculator {
             System.err.println("Ошибка: " + e.getMessage());
         }
     }
-}
 
+    public static int calc(String input) {
+        String[] parts = input.split(" ");
+
+        if (parts.length != 3) {
+            throw new IllegalArgumentException("Неверный формат ввода. Используйте пример: 2 + 3");
+        }
+
+        int a = Integer.parseInt(parts[0]);
+        char operator = parts[1].charAt(0);
+        int b = Integer.parseInt(parts[2]);
+
+        if (a < 1 || a > 10 & b < 1 || b > 10) {
+            throw new IllegalArgumentException("Числа должны быть от 1 до 10.");
+        }
+
+        int result = 0;
+
+        switch (operator) {
+            case '+':
+                result = a + b;
+                break;
+            case '-':
+                result = a - b;
+                break;
+            case '*':
+                result = a * b;
+                break;
+            case '/':
+                if (b == 0) {
+                    throw new ArithmeticException("Деление на ноль невозможно.");
+                }
+                result = a / b;
+                break;
+            default:
+                throw new IllegalArgumentException("Неверная операция. Используйте +, -, *, /.");
+        }
+
+        return result;
+    }
+}
